12cef5d0ace115a3cda27849b5f61f1c7a8c1464
miguelruizc <buumchakalaka@gmail.com>
2024-05-11 01:05:57 +0200

    Add date-fns library to format dates

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import './style.css';
 import {Todo} from './todo.js';
 import {Project} from './project.js';
 import { DOMController } from './DOMcontroller.js';
+import {format} from "date-fns";
 
-const todo1 = new Todo("Do laundry", "Description", "25/12/2025", 1, "Remember the crusty socks", false);
-const todo2 = new Todo("Clean the room", "Description", "35/10/3023", 1, "Clean the room, yep", false);
+const todo1 = new Todo("Do laundry", "Description", new Date(2025, 10, 25), 1, "Remember the crusty socks", false);
+const todo2 = new Todo("Clean the room", "Description", new Date(3045, 12, 1), 1, "Clean the room, yep", false);
 const project1 = new Project("Chores");
 
 project1.todoList.push(todo1);
 project1.todoList.push(todo2);
+todo1.dueDate = format(todo1.dueDate, "dd/MM/yyyy");
 
 console.log(todo1);
 console.log(project1);
